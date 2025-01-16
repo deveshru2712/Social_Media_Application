5110b5ec79b25c4e7155115a7316b73dc24906bf
yashdev2712 <deveshchandra2712@gmail.com>
2025-01-16 18:07:02 +0530

    get and delete notification

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 import connectToDb from "./Db/Db.js";
 
 dotenv.config();
@@ -23,6 +24,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 const port = process.env.PORT;
 
