25a2c75a1812b56483fd9c76b6d8b902b9513a46
yashdev2712 <deveshchandra2712@gmail.com>
2025-01-17 16:45:37 +0530

    frontend part-1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import SignUpPage from "./Pages/Auth/Signup/SignUpPage";
+import LoginPage from "./Pages/Auth/Login/LoginPage";
+import HomePage from "./Pages/Home/HomePage";
+import Sidebar from "./Components/Common/Sidebar";
 
 const App = () => {
-  return <div className="">App</div>;
+  return (
+    <div className="flex max-w-6xl mx-auto">
+      {/* this sidebar is a common component */}
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
