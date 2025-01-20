b25430ad0d0da441a788e4c03d94772c364d7335
Devesh Chandra <yashdev2712@gmail.com>
2025-01-20 10:08:27 +0530

    post image size too big error fix

--- frontend/src/Pages/Home/CreatePost.jsx
+++ frontend/src/Pages/Home/CreatePost.jsx
@@ -16,7 +16,12 @@ const CreatePost = () => {
 
   const queryClient = useQueryClient();
 
-  const { mutate: createPost, isPending } = useMutation({
+  const {
+    mutate: createPost,
+    isPending,
+    isError,
+    error,
+  } = useMutation({
     mutationFn: async ({ text, img }) => {
       try {
         const res = await fetch("/api/posts/create", {
@@ -116,7 +121,7 @@ const CreatePost = () => {
             {isPending ? "Posting..." : "Post"}
           </button>
         </div>
-        {/* {isError && <div className="text-red-500">Something went wrong</div>} */}
+        {isError && <div className="text-red-500">{error.message}</div>}
       </form>
     </div>
   );
