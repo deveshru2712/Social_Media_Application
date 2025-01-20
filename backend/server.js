c2fc524f29d2709a2489317e39ca05ac959a69c3
Devesh Chandra <yashdev2712@gmail.com>
2025-01-20 10:36:41 +0530

    suggested user fetching

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
